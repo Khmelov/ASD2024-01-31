360932963e327e4ba0561473ab808293e2264af4
O-Fedorenko <lis4aliss@yandex.ru>
2024-06-09 18:05:29 +0300

    lesson02 A_VideoRegistrator

--- src/fedorenko/lesson02/A_VideoRegistrator.java
+++ src/fedorenko/lesson02/A_VideoRegistrator.java
@@ -1,6 +1,7 @@
 package fedorenko.lesson02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /*
 Даны события events
@@ -20,25 +21,19 @@ public class A_VideoRegistrator {
     }
     //модификаторы доступа опущены для возможности тестирования
     List<Double> calcStartTimes(double[] events, double workDuration){
-        //events - события которые нужно зарегистрировать
-        //timeWorkDuration время работы видеокамеры после старта
+       Arrays.sort(events); //сортировка массива events
         List<Double> result;
         result = new ArrayList<>();
         int i=0;                              //i - это индекс события events[i]
-        //Комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
-        //Подготовка к жадному поглощению массива событий
-        //hint: сортировка Arrays.sort обеспечит скорость алгоритма
-        //C*(n log n) + C1*n = O(n log n)
-
-        //пока есть незарегистрированные события
-        //получим одно событие по левому краю
-        //и запомним время старта видеокамеры
-        //вычислим момент окончания работы видеокамеры
-        //и теперь пропустим все покрываемые события
-        //за время до конца работы, увеличивая индекс
-
-
-
+       while(i<events.length){
+           double start=events[i]; //получение первого незаргестр события
+           result.add(start); //старт регистратора
+           double end=start+workDuration; //конец работы регистратора
+           //пропуск всех зарегистрированных событий во время работы регистратора
+           while(i<events.length && events[i]<=end){
+               i++;
+           }
+       }
         return result;                        //вернем итог
     }
 }
