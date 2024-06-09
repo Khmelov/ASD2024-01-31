f4de3e334aaaf65fb82d3179a15d1816a29a82b2
O-Fedorenko <lis4aliss@yandex.ru>
2024-06-09 20:03:23 +0300

    lesson04 C_GetInversions

--- src/fedorenko/lesson04/C_GetInversions.java
+++ src/fedorenko/lesson04/C_GetInversions.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
 /*
 Рассчитать число инверсий одномерного массива.
@@ -34,11 +36,70 @@ Sample Output:
 
 
 public class C_GetInversions {
+    static class InversionCounter extends RecursiveTask<Integer> {
+        private final int[] a;
+        private final int start;
+        private final int end;
+
+        public InversionCounter(int[] a, int start, int end) {
+            this.a = a;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (start >= end) {
+                return 0;
+            }
+
+            int mid = start + (end - start) / 2;
+
+            InversionCounter left = new InversionCounter(a, start, mid);
+            InversionCounter right = new InversionCounter(a, mid + 1, end);
+
+            left.fork();
+            int rightResult = right.compute();
+            int leftResult = left.join();
+
+            int mergeResult = mergeAndCountSplitInversions(a, start, mid, end);
+
+            return mergeResult + leftResult + rightResult;
+        }
+
+        private int mergeAndCountSplitInversions(int[] a, int start, int mid, int end) {
+            int[] temp = new int[a.length];
+            System.arraycopy(a, 0, temp, 0, a.length);
+
+            int i = start;
+            int j = mid + 1;
+            int k = start;
+            int splitInversions = 0;
+
+            while (i <= mid && j <= end) {
+                if (temp[i] <= temp[j]) {
+                    a[k++] = temp[i++];
+                } else {
+                    a[k++] = temp[j++];
+                    splitInversions += mid - i + 1;
+                }
+            }
+
+            while (i <= mid) {
+                a[k++] = temp[i++];
+            }
+
+            while (j <= end) {
+                a[k++] = temp[j++];
+            }
+
+            return splitInversions;
+        }
+    }
 
     int calc(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!
         //размер массива
         int n = scanner.nextInt();
         //сам массив
@@ -46,25 +107,16 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
-        //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        // ForkJoinPool для запуска вычислений в нескольких потоках
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        InversionCounter inversionCounter = new InversionCounter(a, 0, n - 1);
+        return forkJoinPool.invoke(inversionCounter);
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson04/dataC.txt");
+        InputStream stream = new FileInputStream(root + "fedorenko/lesson04/dataC.txt");
         C_GetInversions instance = new C_GetInversions();
         //long startTime = System.currentTimeMillis();
         int result = instance.calc(stream);
