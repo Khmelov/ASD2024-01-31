3a905b31815c8d142fea79a648aa0b1293ec45bb
vvvias <pochta7812@gmail.com>
2024-06-10 17:06:01 +0300

    lesson 01-08

---
+++ src/katsuba/lesson05/A_QSort.java
@@ -0,0 +1,70 @@
+package katsuba.lesson05;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A_QSort {
+
+    private class Segment implements Comparable<Segment> {
+        int start;
+        int stop;
+
+        Segment(int start, int stop) {
+            this.start = start;
+            this.stop = stop;
+        }
+
+        @Override
+        public int compareTo(Segment o) {
+            return Integer.compare(this.start, o.start);
+        }
+    }
+
+    int[] getAccessory(InputStream stream) throws FileNotFoundException {
+        Scanner scanner = new Scanner(stream);
+
+        int n = scanner.nextInt(); // Число отрезков
+        Segment[] segments = new Segment[n];
+        int m = scanner.nextInt(); // Число точек
+        int[] points = new int[m];
+        int[] result = new int[m]; // Результат
+
+        // Считываем отрезки
+        for (int i = 0; i < n; i++) {
+            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+        }
+        // Считываем точки
+        for (int i = 0; i < m; i++) {
+            points[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(segments); // Сортируем отрезки по начальным точкам
+
+        // Для каждой точки определяем, сколько отрезков ей принадлежит
+        for (int i = 0; i < m; i++) {
+            int count = 0;
+            for (Segment segment : segments) {
+                if (segment.start > points[i]) break; // Прерываем цикл, если начало следующего отрезка больше текущей точки
+                if (segment.start <= points[i] && points[i] <= segment.stop) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "katsuba/lesson05/dataA.txt");
+        A_QSort instance = new A_QSort();
+        int[] result = instance.getAccessory(stream);
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
+    }
+}
---
+++ src/katsuba/lesson07/A_EditDist.java
@@ -0,0 +1,49 @@
+package katsuba.lesson07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class A_EditDist {
+
+    int getDistanceEdinting(String one, String two) {
+        int m = one.length();
+        int r = two.length();
+        int[][] dp = new int[m + 1][r + 1];
+
+        // Инициализируем базовые случаи
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;  // Преобразование первой строки в пустую строку
+        }
+        for (int j = 0; j <= r; j++) {
+            dp[0][j] = j;  // Преобразование пустой строки во вторую строку
+        }
+
+        // Заполняем таблицу dp
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= r; j++) {
+                if (one.charAt(i - 1) == two.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];  // Если символы равны, операции не требуется
+                } else {
+                    dp[i][j] = Math.min(
+                            Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
+                            dp[i - 1][j - 1] + 1
+                    );  // Минимум среди вставки, удаления и замены
+                }
+            }
+        }
+
+        return dp[m][r];  // Результат находится в правом нижнем углу таблицы
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "katsuba/lesson07/dataABC.txt");
+        A_EditDist instance = new A_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(), scanner.nextLine()));
+    }
+}
