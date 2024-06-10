c58a180da3fda46c1d465b692664b4a0b33531e4
lizakirilenko4@outlook.com <lizakirilenko4@outlook.com>
2024-06-10 14:18:48 +0300

    Lesson3 completed

--- src/by/it/a_khmelev/lesson03/A_Huffman.java
+++ src/by/it/a_khmelev/lesson03/A_Huffman.java
@@ -1,80 +1,44 @@
 package by.it.a_khmelev.lesson03;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-//Lesson 3. A_Huffman.
-//Разработайте метод encode(File file) для кодирования строки (код Хаффмана)
-
-// По данным файла (непустой строке ss длины не более 104104),
-// состоящей из строчных букв латинского алфавита,
-// постройте оптимальный по суммарной длине беспрефиксный код.
-
-// Используйте Алгоритм Хаффмана — жадный алгоритм оптимального
-// безпрефиксного кодирования алфавита с минимальной избыточностью.
-
-// В первой строке выведите количество различных букв kk,
-// встречающихся в строке, и размер получившейся закодированной строки.
-// В следующих kk строках запишите коды букв в формате "letter: code".
-// В последней строке выведите закодированную строку. Примеры ниже
-
-//        Sample Input 1:
-//        a
-//
-//        Sample Output 1:
-//        1 1
-//        a: 0
-//        0
-
-//        Sample Input 2:
-//        abacabad
-//
-//        Sample Output 2:
-//        4 14
-//        a: 0
-//        b: 10
-//        c: 110
-//        d: 111
-//        01001100100111
-
 public class A_Huffman {
 
-    //Изучите классы Node InternalNode LeafNode
+    // Индекс кодов символов
+    static private Map<Character, String> codes = new TreeMap<>();
+
+    // Абстрактный класс элемента дерева
     abstract class Node implements Comparable<Node> {
-        //абстрактный класс элемент дерева
-        //(сделан abstract, чтобы нельзя было использовать его напрямую)
-        //а только через его версии InternalNode и LeafNode
-        private final int frequence; //частота символов
+        private final int frequency; // Частота символов
 
-        //генерация кодов (вызывается на корневом узле
-        //один раз в конце, т.е. после построения дерева)
-        abstract void fillCodes(String code);
-
-        //конструктор по умолчанию
-        private Node(int frequence) {
-            this.frequence = frequence;
+        // Конструктор по умолчанию
+        private Node(int frequency) {
+            this.frequency = frequency;
         }
 
-        //метод нужен для корректной работы узла в приоритетной очереди
-        //или для сортировок
+        // Метод нужен для корректной работы узла в приоритетной очереди
+        // или для сортировок
         @Override
         public int compareTo(Node o) {
-            return Integer.compare(frequence, o.frequence);
+            return Integer.compare(frequency, o.frequency);
         }
+
+        // Генерация кодов (вызывается на корневом узле
+        // один раз в конце, т.е. после построения дерева)
+        abstract void fillCodes(String code);
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////
-    //расширение базового класса до внутреннего узла дерева
+    // Расширение базового класса до внутреннего узла дерева
     private class InternalNode extends Node {
-        //внутренный узел дерева
-        Node left;  //левый ребенок бинарного дерева
-        Node right; //правый ребенок бинарного дерева
+        // Внутренний узел дерева
+        Node left;  // Левый ребенок бинарного дерева
+        Node right; // Правый ребенок бинарного дерева
 
-        //для этого дерева не существует внутренних узлов без обоих детей
-        //поэтому вот такого конструктора будет достаточно
+        // Для этого дерева не существует внутренних узлов без обоих детей
+        // Поэтому такого конструктора будет достаточно
         InternalNode(Node left, Node right) {
-            super(left.frequence + right.frequence);
+            super(left.frequency + right.frequency);
             this.left = left;
             this.right = right;
         }
@@ -84,77 +48,72 @@ public class A_Huffman {
             left.fillCodes(code + "0");
             right.fillCodes(code + "1");
         }
-
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////
-    //расширение базового класса до листа дерева
+    // Расширение базового класса до листа дерева
     private class LeafNode extends Node {
-        //лист
-        char symbol; //символы хранятся только в листах
+        // Лист
+        char symbol; // Символы хранятся только в листах
 
-        LeafNode(int frequence, char symbol) {
-            super(frequence);
+        LeafNode(int frequency, char symbol) {
+            super(frequency);
             this.symbol = symbol;
         }
 
         @Override
         void fillCodes(String code) {
-            //добрались до листа, значит рекурсия закончена, код уже готов
-            //и можно запомнить его в индексе для поиска кода по символу.
+            // Добрались до листа, значит рекурсия закончена, код уже готов
+            // И можно запомнить его в индексе для поиска кода по символу.
             codes.put(this.symbol, code);
         }
     }
 
-    //индекс данных из листьев
-    static private Map<Character, String> codes = new TreeMap<>();
-
-
-    //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+    // Метод для кодирования строки по алгоритму Хаффмана
     String encode(File file) throws FileNotFoundException {
-        //прочитаем строку для кодирования из тестового файла
+        // Прочитаем строку для кодирования из тестового файла
         Scanner scanner = new Scanner(file);
         String s = scanner.next();
+        scanner.close();
 
-        //все комментарии от тестового решения были оставлены т.к. это задание A.
-        //если они вам мешают их можно удалить
-
+        // Подсчитаем частоту каждого символа в строке
         Map<Character, Integer> count = new HashMap<>();
-        //1. переберем все символы по очереди и рассчитаем их частоту в Map count
-            //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
+        for (char ch : s.toCharArray()) {
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        }
 
-        //2. перенесем все символы в приоритетную очередь в виде листьев
+        // Создадим листы для каждого символа и добавим их в приоритетную очередь
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        for (Map.Entry<Character, Integer> entry : count.entrySet()) {
+            priorityQueue.add(new LeafNode(entry.getValue(), entry.getKey()));
+        }
 
-        //3. вынимая по два узла из очереди (для сборки родителя)
-        //и возвращая этого родителя обратно в очередь
-        //построим дерево кодирования Хаффмана.
-        //У родителя частоты детей складываются.
+        // Построим дерево Хаффмана
+        while (priorityQueue.size() > 1) {
+            Node left = priorityQueue.poll();
+            Node right = priorityQueue.poll();
+            priorityQueue.add(new InternalNode(left, right));
+        }
 
-        //4. последний из родителей будет корнем этого дерева
-        //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
-        StringBuilder sb = new StringBuilder();
-        //.....
+        // Получим корень дерева
+        Node root = priorityQueue.poll();
+        if (root != null) {
+            root.fillCodes("");
+        }
 
-        return sb.toString();
-        //01001100100111
-        //01001100100111
+        // Создадим закодированную строку на основе индекса кодов
+        StringBuilder encodedString = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            encodedString.append(codes.get(ch));
+        }
+
+        return encodedString.toString();
     }
-    //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-
 
     public static void main(String[] args) throws FileNotFoundException {
-        String root = System.getProperty("user.dir") + "/src/";
+        String root = System.getProperty("user.dir") + "/ASD2024-01-31/src/";
         File f = new File(root + "by/it/a_khmelev/lesson03/dataHuffman.txt");
         A_Huffman instance = new A_Huffman();
-        long startTime = System.currentTimeMillis();
         String result = instance.encode(f);
-        long finishTime = System.currentTimeMillis();
-        System.out.printf("%d %d\n", codes.size(), result.length());
-        for (Map.Entry<Character, String> entry : codes.entrySet()) {
-            System.out.printf("%s: %s\n", entry.getKey(), entry.getValue());
-        }
         System.out.println(result);
     }
-
 }
--- src/by/it/a_khmelev/lesson03/B_Huffman.java
+++ src/by/it/a_khmelev/lesson03/B_Huffman.java
@@ -1,70 +1,49 @@
 package by.it.a_khmelev.lesson03;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
-// Lesson 3. B_Huffman.
-// Восстановите строку по её коду и беспрефиксному коду символов.
-
-// В первой строке входного файла заданы два целых числа
-// kk и ll через пробел — количество различных букв, встречающихся в строке,
-// и размер получившейся закодированной строки, соответственно.
-//
-// В следующих kk строках записаны коды букв в формате "letter: code".
-// Ни один код не является префиксом другого.
-// Буквы могут быть перечислены в любом порядке.
-// В качестве букв могут встречаться лишь строчные буквы латинского алфавита;
-// каждая из этих букв встречается в строке хотя бы один раз.
-// Наконец, в последней строке записана закодированная строка.
-// Исходная строка и коды всех букв непусты.
-// Заданный код таков, что закодированная строка имеет минимальный возможный размер.
-//
-//        Sample Input 1:
-//        1 1
-//        a: 0
-//        0
-
-//        Sample Output 1:
-//        a
-
-
-//        Sample Input 2:
-//        4 14
-//        a: 0
-//        b: 10
-//        c: 110
-//        d: 111
-//        01001100100111
-
-//        Sample Output 2:
-//        abacabad
 
 public class B_Huffman {
 
     String decode(File file) throws FileNotFoundException {
-        StringBuilder result=new StringBuilder();
-        //прочитаем строку для кодирования из тестового файла
+        StringBuilder result = new StringBuilder();
+
+        // Прочитаем данные из файла
         Scanner scanner = new Scanner(file);
-        Integer count = scanner.nextInt();
-        Integer length = scanner.nextInt();
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-        //тут запишите ваше решение
+        int count = scanner.nextInt(); // Количество различных букв
+        int length = scanner.nextInt(); // Размер получившейся закодированной строки
+        Map<String, String> map = new HashMap<>(); // Хранение кодов и соответствующих символов
 
+        // Считываем коды букв и сохраняем их в карте
+        for (int i = 0; i < count; i++) {
+            String letter = scanner.next().substring(0, 1); // Получаем символ
+            String code = scanner.next(); // Получаем код
+            map.put(code, letter); // Добавляем в карту
+        }
 
+        // Считываем закодированную строку
+        String encodedString = scanner.next();
 
+        // Декодируем строку по коду Хаффмана
+        StringBuilder currentCode = new StringBuilder(); // Хранение текущего кода
+        for (int i = 0; i < length; i++) {
+            currentCode.append(encodedString.charAt(i)); // Добавляем символ к текущему коду
+            if (map.containsKey(currentCode.toString())) { // Если текущий код существует в карте
+                result.append(map.get(currentCode.toString())); // Добавляем символ в результат
+                currentCode = new StringBuilder(); // Сбрасываем текущий код
+            }
+        }
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-        return result.toString(); //01001100100111
+        return result.toString();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        String root = System.getProperty("user.dir") + "/src/";
+        String root = System.getProperty("user.dir") + "/ASD2024-01-31/src/";
         File f = new File(root + "by/it/a_khmelev/lesson03/encodeHuffman.txt");
         B_Huffman instance = new B_Huffman();
         String result = instance.decode(f);
         System.out.println(result);
     }
-
-
 }
