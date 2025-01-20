529445ff99c20eca860824269d5c6bf58cd1f43b
SuPolinA <su582004@gmail.com>
2025-01-21 00:41:38 +0300

    lesson9

---
+++ src/by/it/group310971/supranovich/lesson09/ListB.java
@@ -0,0 +1,256 @@
+package by.it.group310971.supranovich.lesson09;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListB<E> implements List<E> {
+
+    private Object[] elements;
+    private int size;
+
+    // Constructor to initialize the list with an initial capacity
+    public ListB() {
+        elements = new Object[10];  // Initial capacity of 10
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = e;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+        E removedElement = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null;  // Null out the last element
+        return removedElement;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkIndexForAdd(index);
+        if (size == elements.length) {
+            resize();
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndex(index);
+        E oldElement = (E) elements[index];
+        elements[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+        return (E) elements[index];
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (elements[i].equals(o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c) {
+            add(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        for (E e : c) {
+            add(index++, e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object o : c) {
+            while (remove(o)) {
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<E> iter = iterator();
+        while (iter.hasNext()) {
+            if (!c.contains(iter.next())) {
+                iter.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        checkIndex(fromIndex);
+        checkIndex(toIndex);
+        ListB<E> subList = new ListB<>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            subList.add((E) elements[i]);
+        }
+        return subList;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            return (T[]) java.util.Arrays.copyOf(elements, size, a.getClass());
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return java.util.Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public E next() {
+                return (E) elements[index++];
+            }
+        };
+    }
+
+    // Helper method to resize the array when it's full
+    private void resize() {
+        elements = java.util.Arrays.copyOf(elements, elements.length * 2);
+    }
+
+    // Helper method to check index validity
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    // Helper method to check index validity for adding
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+}
