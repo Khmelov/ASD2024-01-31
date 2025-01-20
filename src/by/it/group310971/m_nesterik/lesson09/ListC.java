5fd0de793331bf5b030d76932c3c007313f95b63
MafllGaale <93854605+MafllGaale@users.noreply.github.com>
2025-01-21 02:22:54 +0300

    Add files via upload

---
+++ src/by/it/group310971/m_nesterik/lesson09/ListC.java
@@ -0,0 +1,265 @@
+package by.it.m_nesterik.lesson09;
+
+import java.util.*;
+
+public class ListC<E> implements List<E> {
+
+    // Внутренний массив для хранения данных
+    private Object[] elements = new Object[10];
+    private int size = 0;
+
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    //////               Обязательные к реализации методы             ///////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean add(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+        E oldValue = (E) elements[index];
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
+        return oldValue;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        checkIndexForAdd(index);
+        ensureCapacity();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index >= 0) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndex(index);
+        E oldValue = (E) elements[index];
+        elements[index] = element;
+        return oldValue;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+        return (E) elements[index];
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    //////               Опциональные к реализации методы             ///////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object element : c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        for (E element : c) {
+            add(element);
+            modified = true;
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        checkIndexForAdd(index);
+        boolean modified = false;
+        for (E element : c) {
+            add(index++, element);
+            modified = true;
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object element : c) {
+            while (contains(element)) {
+                remove(element);
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elements[i])) {
+                remove(i--);
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    // Преобразование списка в массив
+    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elements, size);
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            return (T[]) Arrays.copyOf(elements, size, a.getClass());
+        }
+        System.arraycopy(elements, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    // Работа с подсписками
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        if (fromIndex < 0 || toIndex > size || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException();
+        }
+        ListC<E> subList = new ListC<>();
+        for (int i = fromIndex; i < toIndex; i++) {
+            subList.add((E) elements[i]);
+        }
+        return subList;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        throw new UnsupportedOperationException("ListIterator is not supported.");
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        throw new UnsupportedOperationException("ListIterator is not supported.");
+    }
+
+    // Итератор для списка
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) elements[cursor++];
+            }
+        };
+    }
+
+    // Увеличение емкости массива
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+    }
+
+    // Проверка индекса на допустимость
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    // Проверка индекса для добавления
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+}
---
+++ src/by/it/group310971/m_nesterik/lesson14/PointsA.java
@@ -0,0 +1,96 @@
+package by.it.m_nesterik.lesson14;
+
+import java.util.*;
+
+public class PointsA {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Чтение входных данных
+        double distanceRequired = scanner.nextDouble(); // Максимальное расстояние
+        int pointCount = scanner.nextInt(); // Количество точек
+
+        DSU dsu = new DSU(pointCount);
+        int[][] points = new int[pointCount][3];
+
+        // Чтение координат точек
+        for (int i = 0; i < pointCount; i++) {
+            points[i][0] = scanner.nextInt();
+            points[i][1] = scanner.nextInt();
+            points[i][2] = scanner.nextInt();
+        }
+
+        // Объединение точек в кластеры
+        for (int i = 0; i < pointCount; i++) {
+            for (int j = i + 1; j < pointCount; j++) {
+                if (calculateDistance(points[i], points[j]) < distanceRequired) {
+                    dsu.union(i, j);
+                }
+            }
+        }
+
+        // Подсчет размеров кластеров
+        Map<Integer, Integer> clusterSizes = new HashMap<>();
+        for (int i = 0; i < pointCount; i++) {
+            int root = dsu.find(i);
+            clusterSizes.put(root, clusterSizes.getOrDefault(root, 0) + 1);
+        }
+
+        // Получение и сортировка размеров кластеров
+        List<Integer> sizes = new ArrayList<>(clusterSizes.values());
+        Collections.sort(sizes, Collections.reverseOrder()); // Сортировка по убыванию
+
+        // Вывод размеров кластеров
+        for (int size : sizes) {
+            System.out.print(size + " ");
+        }
+    }
+
+    // Метод для вычисления евклидова расстояния между двумя точками
+    private static double calculateDistance(int[] point1, int[] point2) {
+        return Math.sqrt(
+                Math.pow(point1[0] - point2[0], 2) +
+                        Math.pow(point1[1] - point2[1], 2) +
+                        Math.pow(point1[2] - point2[2], 2)
+        );
+    }
+
+    // Реализация DSU (Disjoint Set Union)
+    static class DSU {
+        private int[] parent;
+        private int[] rank;
+
+        public DSU(int size) {
+            parent = new int[size];
+            rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // Сжатие пути
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+}
