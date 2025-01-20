13fc4ca54ad918c5c360053c7dcd616f3f72e733
Alexander Mukhanyuk <mukhanyuk04@gmail.com>
2025-01-20 22:40:35 +0300

    Практическая работа 9

---
+++ src/by/it/group310971/mukhanyuk/lesson09/ListB.java
@@ -0,0 +1,263 @@
+package by.it.group310971.mukhanyuk.lesson09;
+
+import java.util.*;
+
+public class ListB<E> implements List<E> {
+    //Создайте аналог списка БЕЗ использования других классов СТАНДАРТНОЙ БИБЛИОТЕКИ
+
+    private Object[] elements; // Массив для хранения элементов
+    private int size;          // Текущий размер списка
+
+    public ListB() {
+        elements = new Object[10]; // Начальная емкость массива
+        size = 0;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    //////               Обязательные к реализации методы             ///////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            sb.append(elements[i]);
+            if (i < size - 1) sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (size == elements.length) {
+            grow();
+        }
+        elements[size++] = e;
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+        E removedElement = (E) elements[index];
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null;
+        return removedElement;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    //////               Опциональные к реализации методы             ///////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void add(int index, E element) {
+        checkIndexForAdd(index);
+        if (size == elements.length) {
+            grow();
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndex(index);
+        E oldElement = (E) elements[index];
+        elements[index] = element;
+        return oldElement;
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        for (int i = 0; i < size; i++) {
+            if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+        return (E) elements[index];
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        for (int i = size - 1; i >= 0; i--) {
+            if ((o == null && elements[i] == null) || (o != null && o.equals(elements[i]))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        for (Object o : c) {
+            if (!contains(o)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        for (E e : c) {
+            add(e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        checkIndexForAdd(index);
+        for (E e : c) {
+            add(index++, e);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object o : c) {
+            while (remove(o)) {
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elements[i])) {
+                remove(i--);
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException("Подсписки не поддерживаются.");
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        throw new UnsupportedOperationException("Итераторы списка не поддерживаются.");
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        throw new UnsupportedOperationException("Итераторы списка не поддерживаются.");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        throw new UnsupportedOperationException("Преобразования списка в массив заданного типа не поддерживается.");
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size];
+        System.arraycopy(elements, 0, result, 0, size);
+        return result;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    ////////        Эти методы имплементировать необязательно    ////////////
+    ////////        но они будут нужны для корректной отладки    ////////////
+    /////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) elements[currentIndex++];
+            }
+        };
+    }
+
+    private void grow() {
+        Object[] newElements = new Object[elements.length + elements.length / 2];
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
+        elements = newElements;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private void checkIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+}
